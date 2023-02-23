121fde2ecdb98c23b96281f225b6f5fe6e1a1b80
coding <mjr.rezai@gmail.com>
2023-02-23 16:03:36 +0100

    first commit

---
+++ lec1_sum.py
@@ -0,0 +1,5 @@
+# How to add two numbers together
+num1 = int(input("Enter first number here:"))
+num2 = int(input("Enter second number here:"))
+sum = num1 + num2
+print("The sum of the two numbeer is",sum)
---
+++ lec3_square.py
@@ -0,0 +1,14 @@
+
+#Solution-1 (using Exponentiontion)
+num1 = int(input("Enter a number here:"))
+sr = num1 ** (1/2)
+print("The square root of the given number is:",sr)
+
+# Solution-2 (Using Math Module)
+
+import math
+
+num = int(input("enter a number here:"))
+
+sr1 = math.sqrt(num)
+print("The second methode square root of the given number is:",sr1)
